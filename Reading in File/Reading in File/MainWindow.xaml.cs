d0e1adb9be7580c4e2241c3ca91d26c361b7e2b7
Matthew Brinkmeyer <brinkmeyermatthew@ou.edu>
2022-02-08 13:02:44 -0600

    idk

---
+++ Reading in File/Reading in File/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Reading_in_File
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string[] linesoffile = File.ReadAllLines("SalesJan2009.csv");
+
+            for (int i = 0; i < linesoffile.Length; i++)
+            {
+                // Transaction_date,Product,Price,Payment_Type,Name,City,State,Country,Account_Created,Last_Login,Latitude,Longitude
+                //     0                  1    2     3             4   5    6      7         8              9          10      11
+
+                string[] piece = linesoffile[i].Split(',');
+                SalesDataItem item = new SalesDataItem();
+               // item.Transaction_Date = Convert.ToDateTime(piece[0]);
+                item.Product = piece[1];
+                item.Price = Convert.ToDouble(piece[2]);
+                item.Payment = piece[3];
+                item.Name = piece[4];
+                item.City = piece[5];
+                item.State = piece[6];
+                item.Country = piece[7];
+                item.Account_Created = piece[8];
+                item.Last_Login = piece[9];
+                item.Latitude = Convert.ToDouble( piece[10]);
+                item.Longitude = Convert.ToDouble( piece[11]);
+
+                lstBoxSalesData.Items.Add(item);
+            }
+        }
+
+        private void cmbCard_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+    }
+}
---
+++ Reading in File/Reading in File/SalesDataItem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reading_in_File
+{
+    internal class SalesDataItem
+    {
+        public DateTime Transaction_Date { get; set; }
+        public string Product { get; set; }
+        public double Price { get; set; }
+        public string Payment { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string Account_Created { get; set; }
+        public string Last_Login { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public SalesDataItem()
+        {
+            Transaction_Date = DateTime.Now;
+            Product = string.Empty;
+            Price = 0;
+            Payment = string.Empty;
+            Name = string.Empty;
+            City = string.Empty;
+            State = string.Empty;
+            Country = string.Empty;
+            Account_Created = string.Empty;
+            Last_Login = string.Empty;
+            Latitude = 0;
+            Longitude = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{Product} cost {Price.ToString("C")} and was bought by {Name} using {Payment}";
+        }
+    }
+}

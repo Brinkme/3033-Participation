1956667eeb89c091a148316c11da49d5d7cd1f74
Matthew Brinkmeyer <brinkmeyermatthew@ou.edu>
2022-02-08 12:12:20 -0600

    done

---
+++ test2/test2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Are you Connor");
+string answer;
+answer = Console.ReadLine().ToLower();
+if (answer[0] == 'y' )
+{
+    Console.WriteLine("Fuck you!");
+}
+else
+{
+    Console.WriteLine("What is your name?");
+    answer = Console.ReadLine();
+
+    Console.WriteLine($"Well, {answer} fuck you!");
+}

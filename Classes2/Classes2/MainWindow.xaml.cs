a806df35d2c349ad2ca90367d85d5d22a9c46d66
Matthew Brinkmeyer <brinkmeyermatthew@ou.edu>
2022-02-10 13:14:43 -0600

    Done

---
+++ Classes2/Classes2/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Classes2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtBoxMan.Clear();
+            txtBoxName.Clear();
+            txtBoxPrice.Clear();
+            txtBoxURL.Clear();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Toy thing = new Toy();
+            if (string.IsNullOrWhiteSpace(txtBoxMan.Text) == false)
+            {
+            thing.Manufactuer = txtBoxMan.Text;
+            }
+            else
+            {
+                MessageBox.Show("Sorry, that is invalid enter a Manufacturer");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(txtBoxName.Text) == false)
+            {
+            thing.Name = txtBoxName.Text;
+            }
+            else
+            {
+                MessageBox.Show("Sorry, that is invalid enter a Name");
+                return;
+            }
+
+            if (double.TryParse(txtBoxPrice.Text, out double price) == true)
+            {
+                thing.Price = price;
+            }
+            else
+            {
+                MessageBox.Show("Sorry, that is invalid enter a Price");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(txtBoxURL.Text) == false)
+            {
+            thing.Image = txtBoxURL.Text;
+            }
+            else
+            {
+                MessageBox.Show("Sorry, that is invalid enter a URL");
+                return;
+            }
+
+            lstBoxAdd.Items.Add(thing);
+        }
+
+        private void lstBoxAdd_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Toy selectedToy = (Toy)lstBoxAdd.SelectedItem;
+
+            //MessageBox.Show();
+        }
+    }
+}
---
+++ Classes2/Classes2/Toy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes2
+{
+    public class Toy
+    {
+        public string Manufactuer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        private string Aisle { get; set; }
+
+        public Toy()
+        {
+            Manufactuer = string.Empty;
+            Name = string.Empty;
+            Price = 0.0;
+            Image = string.Empty;
+            Aisle = string.Empty;
+        }
+        public string GetAisle()
+        {
+            string M = Manufactuer.ToUpper();
+            string thing = $"{M[0]}, {Price.ToString("")}";
+            return thing;
+        }
+        public override string ToString()
+        {
+            string o = Manufactuer + "-" + Name;
+            return o;
+        }
+    }
+}

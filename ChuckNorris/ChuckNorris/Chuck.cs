be8dcd09a04d1813dca14c30b42ba34633af1947
Matthew Brinkmeyer <brinkmeyermatthew@ou.edu>
2022-02-23 22:16:09 -0600

    started

---
+++ ChuckNorris/ChuckNorris/Chuck.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckNorris
+{
+    public class Chuck
+    {
+        public List<Category> categories { get; set; }
+
+        public string created_at { get; set; }
+        public string icon_url { get; set; }
+        public string id { get; set; }
+        public string updated_at { get; set; }
+        public string url { get; set; }
+        public string value { get; set; }
+
+    }
+
+    public class Category
+    {
+   public string animal         {get; set;}
+   public string career         {get; set;}
+   public string celebrity      {get; set;}
+   public string dev            {get; set;}
+   public string fashion        {get; set;}
+   public string food           {get; set;}
+   public string history        {get; set;}
+   public string money          {get; set;}
+   public string movie          {get; set;}
+   public string music          {get; set;}
+   public string political      {get; set;}
+   public string religion       {get; set;}
+   public string science        {get; set;}
+   public string sport          {get; set;}
+   public string travel         { get; set; }
+
+   //public string explicit       {get; set;}
+
+
+
+
+
+
+    }
+}
---
+++ ChuckNorris/ChuckNorris/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChuckNorris
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            cmbBoxCat.Items.Add("All");
+            cmbBoxCat.SelectedIndex = 0;
+        }
+
+        private void btnClick_Click(object sender, RoutedEventArgs e)
+        {
+            string URL = "https://api.chucknorris.io/jokes/random";
+
+            string json;
+
+            using (var client = new HttpClient())
+            {
+                json = client.GetStringAsync(URL).Result;
+
+                Chuck api = JsonConvert.DeserializeObject<Chuck>(json);
+
+                lstBoxJoke1.Items.Add(api.value);
+            }
+        }
+
+        private void lstBoxJoke1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            //lstBoxJoke1.SelectedItem = thing;
+
+            Chuck selected = (Chuck)lstBoxJoke1.SelectedItem;
+
+            imgChuck.Source = new BitmapImage(new Uri(selected.icon_url));
+        }
+    }
+}

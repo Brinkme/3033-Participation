a9e9d45ce494b5af1b097c18fbfd82f116aa0ced
Matthew Brinkmeyer <brinkmeyermatthew@ou.edu>
2022-03-01 16:57:47 -0600

    L

--- Reading in File/Reading in File/MainWindow.xaml.cs
+++ Reading in File/Reading in File/MainWindow.xaml.cs
@@ -21,38 +21,61 @@ namespace Reading_in_File
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<SalesDataItem> datas = new List<SalesDataItem>();
         public MainWindow()
         {
             InitializeComponent();
+            cmbCard.SelectedIndex = 0;
 
             string[] linesoffile = File.ReadAllLines("SalesJan2009.csv");
 
-            for (int i = 0; i < linesoffile.Length; i++)
+            for (int i = 1; i < linesoffile.Length; i++)
             {
                 // Transaction_date,Product,Price,Payment_Type,Name,City,State,Country,Account_Created,Last_Login,Latitude,Longitude
                 //     0                  1    2     3             4   5    6      7         8              9          10      11
 
                 string[] piece = linesoffile[i].Split(',');
+                DateTime temp;
                 SalesDataItem item = new SalesDataItem();
-               // item.Transaction_Date = Convert.ToDateTime(piece[0]);
-                item.Product = piece[1];
+
+                if (DateTime.TryParse(piece[0], out temp))
+                {
+                    item.Transaction_Date = temp;   
+                }
+                item.Product = piece[1].Trim();
                 item.Price = Convert.ToDouble(piece[2]);
-                item.Payment = piece[3];
-                item.Name = piece[4];
-                item.City = piece[5];
-                item.State = piece[6];
-                item.Country = piece[7];
-                item.Account_Created = piece[8];
-                item.Last_Login = piece[9];
-                item.Latitude = Convert.ToDouble( piece[10]);
-                item.Longitude = Convert.ToDouble( piece[11]);
+                item.Payment_Type = piece[3].Trim();
+                item.Name = piece[4].Trim();
+                item.City = piece[5].Trim();
+                item.State = piece[6].Trim();
+                item.Country = piece[7].Trim();
+                item.Account_Created = piece[8].Trim();
+                item.Last_Login = piece[9].Trim();
+                item.Latitude = piece[10].Trim();
+                item.Longitude = piece[11].Trim();
+
+                if (cmbCard.Items.Contains(item.Payment_Type) == false)
+                {
+                    cmbCard.Items.Add(item.Payment_Type);
+                }
 
                 lstBoxSalesData.Items.Add(item);
+                datas.Add(item);  
             }
         }
 
         private void cmbCard_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            string payment = cmbCard.SelectedItem.ToString();
+            lstBoxSalesData.Items.Clear();
+
+            foreach (var item in datas)
+            {
+                if (item.Payment_Type == payment)
+                {
+                    lstBoxSalesData.Items.Add(item);
+                }
+            }
         }
     }
 }
--- Reading in File/Reading in File/SalesDataItem.cs
+++ Reading in File/Reading in File/SalesDataItem.cs
@@ -6,40 +6,40 @@ using System.Threading.Tasks;
 
 namespace Reading_in_File
 {
-    internal class SalesDataItem
+    public class SalesDataItem
     {
         public DateTime Transaction_Date { get; set; }
         public string Product { get; set; }
         public double Price { get; set; }
-        public string Payment { get; set; }
+        public string Payment_Type { get; set; }
         public string Name { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public string Country { get; set; }
         public string Account_Created { get; set; }
         public string Last_Login { get; set; }
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
 
         public SalesDataItem()
         {
             Transaction_Date = DateTime.Now;
             Product = string.Empty;
             Price = 0;
-            Payment = string.Empty;
+            Payment_Type = string.Empty;
             Name = string.Empty;
             City = string.Empty;
             State = string.Empty;
             Country = string.Empty;
             Account_Created = string.Empty;
             Last_Login = string.Empty;
-            Latitude = 0;
-            Longitude = 0;
+            Latitude = string.Empty;
+            Longitude = string.Empty;
         }
 
         public override string ToString()
         {
-            return $"{Product} cost {Price.ToString("C")} and was bought by {Name} using {Payment}";
+            return $"{Product} cost {Price.ToString("C")} and was bought by {Name} using {Payment_Type}";
         }
     }
 }
